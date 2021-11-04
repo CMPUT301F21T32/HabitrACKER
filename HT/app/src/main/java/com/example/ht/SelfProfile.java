b967ca128a7210be47e14ce8d07bdb3758d9baa1
Josh Antoniuk <joshantoniuk@gmail.com>
2021-11-03 19:30:49 -0600

    More Javadoc/cleanup

--- HT/app/src/main/java/com/example/ht/SelfProfile.java
+++ HT/app/src/main/java/com/example/ht/SelfProfile.java
@@ -1,9 +1,6 @@
 // CODED BY JOSH
 package com.example.ht;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +8,12 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class SelfProfile extends AppCompatActivity {
 
@@ -27,6 +22,16 @@ public class SelfProfile extends AppCompatActivity {
     ArrayAdapter<Habit> habitAdapter;
     // Declarations for the xml list, list that contains habit items, and the adapter for the array
 
+    /**
+     * This starts the activity, and inflates the self_profile
+     * layout. NOTE: in AndroidManifest.xml, I set this screen
+     * to be the main (first) screen, to change this,
+     * take out the <activity></activity> with .SelfProfile
+     * OUTSIDE of the <intent-filter></intent-filter>
+     * you also might have to change the android:parent tag
+     * on some of the other activities.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +41,9 @@ public class SelfProfile extends AppCompatActivity {
 
     /**
      * This function takes a habit, and adds it
-     * to the list, updating the adapter
+     * to the list, updating the adapter.
+     * Unfortunately I could not just add the item in
+     * populateList() because it wont work.
      * @param habit
      */
     public void addHabitToList(Habit habit) {
@@ -44,9 +51,12 @@ public class SelfProfile extends AppCompatActivity {
         Log.d("LIST CHECK", habitList.get(0).getName());
 
         mainList = findViewById(R.id.habit_list);
+        //xml list reference
+
         habitAdapter = new CustomList(this, habitList);
         mainList.setAdapter(habitAdapter);
         habitAdapter.notifyDataSetChanged();
+        // update list
     }
 
     /**
@@ -65,6 +75,8 @@ public class SelfProfile extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         habitList.clear();
                         for (QueryDocumentSnapshot document : task.getResult()) {
+
+                            // Get the attributes from each habit in the database
                             String title = document.getData().get("name").toString();
                             String description = document.getData().get("description").toString();
                             String hour = document.getData().get("hour").toString();
@@ -73,8 +85,10 @@ public class SelfProfile extends AppCompatActivity {
                             String selectedDays = document.getData().get("selectedDays").toString();
                             String username = document.getData().get("username").toString();
 
+                            // Create new habit and add to the list!
                             Habit newHabit = new Habit(title, description, selectedDays, hour, minute, date, username);
                             addHabitToList(newHabit);
+
                             Log.d("HABIT:", title);
                         }
                     } else {
@@ -85,12 +99,16 @@ public class SelfProfile extends AppCompatActivity {
 
     /**
      * This function starts an intent (AddActivity)
-     * and adds the habit to the database.
+     * and adds the habit to the database. This
+     * is called when "Add Habit" Button is
+     * tapped.
      * @param view
      */
     public void addHabit(View view) {
+        // Start AddActivity Page
         Intent intent = new Intent(this, AddActivity.class);
         startActivity(intent);
+
         Log.d("POPULATING:", "\n");
     }
 
