e7ff139ac5a2534f3aa318af87c5f34a23a037ee
jacquelinedenise <jazarcon@ualberta.ca>
2021-11-05 09:35:58 -0600

    Update

--- HT/app/src/test/java/com/example/ht/TestEditDeleteEvent.java
+++ HT/app/src/test/java/com/example/ht/TestEditDeleteEvent.java
@@ -1,38 +1,31 @@
 package com.example.ht;
 
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestEditDeleteEvent {
-    EditDeleteEvent testEvent1;
-    EditDeleteEvent testEvent2;
 
-    @Before
-    public void createEvent(){
-        //Create existing events to test on
-        testEvent1= new EditDeleteEvent();
-        testEvent2= new EditDeleteEvent();
+    HabitEvent testHabit1= new HabitEvent("habitID", "description that's also comments must be under 20, is not",
+            "hour", "minute", "name", "username");
+    HabitEvent testEvent2= new HabitEvent("habitID", "is under 20",
+            "hour", "minute", "name", "username");
+
+    //Need to merge with Deepak
+    @Test
+    public void testComments1(){
+        // Check if there are 20 characters in comment after edit
+        // Should pass
+        int commentLength= testEvent1.comment.length();
+        assertEquals(commentLength, commentLength<20);
     }
 
     @Test
-    public void testEdit1(){
+    public void testComments2(){
+        // Check if there are 20 characters in comments after edit
+        //Should fail
+        int commentLength= testHabit2.comment.length();
+        assertEquals(commentLength, commentLength>20);
 
     }
-
-    @Test
-    public void testEdit2(){
-
-    }
-
-    @Test
-    public void testDelete1(){
-
-    }
-
-    @Test
-    public void testDelete2(){
-
-    }
-
-
 }
