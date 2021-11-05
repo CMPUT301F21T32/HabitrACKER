2d8dff24011a20692c2ca641f7af16fd9e32e5f6
colerjs <91639874+colerjs@users.noreply.github.com>
2021-11-04 18:12:31 -0600

    Revert "Josh"

--- HT/app/src/main/java/com/example/ht/SelfProfile.java
+++ HT/app/src/main/java/com/example/ht/SelfProfile.java
@@ -1,27 +1,19 @@
-// CODED BY JOSH
 package com.example.ht;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-
 import android.widget.ImageButton;
-
 import android.widget.ListView;
 import android.widget.TextView;
 
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -30,135 +22,158 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class SelfProfile extends AppCompatActivity {
 
     ListView mainList;
     ArrayList<Habit> habitList = new ArrayList<>();
     ArrayAdapter<Habit> habitAdapter;
-
-    boolean isDeleting = false;
-    // Declarations for the xml list, list that contains habit items, and the adapter for the array
-
-    /**
-     * This starts the activity, and inflates the self_profile
-     * layout. NOTE: in AndroidManifest.xml, I set this screen
-     * to be the main (first) screen, to change this,
-     * take out the <activity></activity> with .SelfProfile
-     * OUTSIDE of the <intent-filter></intent-filter>
-     * you also might have to change the android:parent tag
-     * on some of the other activities.
-     * @param savedInstanceState
-     */
+    //initialize position
+    int position;
+    ListView v_habitList;
+    Button deleteHabitButton;
+    FirebaseFirestore db;
+    final String TAG = "Sample";
+    String username;
+    TextView nameLabel;
+    TextView usernameLabel;
+    ImageButton detailsButton = findViewById(R.id.details_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.self_profile);
         populateList();
+        //initialize list of habits view
+        v_habitList = findViewById(R.id.habit_list);
+        deleteHabitButton=findViewById(R.id.deleteHabitButton);
+        db = FirebaseFirestore.getInstance();
 
-        Button deleteButton = findViewById(R.id.delete_button);
+        usernameLabel = findViewById(R.id.username);
+        nameLabel = findViewById(R.id.full_name);
 
-        // Setting up list item click
-        mainList = findViewById(R.id.habit_list);
-        mainList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            /**
-             * This function sets an onItemClickAdapter for the listview
-             * NOTE: THIS ONLY WORKS WHEN THE LINERAR LAYOUT OF THE CELL
-             * CONTENT HAS android:descendantFocusability = "blocksDescendants"
-             * After the button (FOLLOW REQUESTS FOR NOW) is clicked, you
-             * are in deletion mode. You can now click on any list item to
-             * delete it. After it is deleted from the database, it is removed
-             * from the list, and we update the adapter accordingly.
-             * This is all inside a condition
-             * @param adapterView
-             * @param view
-             * @param i
-             * @param l
-             */
+        Intent intent = getIntent();
+        username = intent.getStringExtra("USERNAME");
+
+        DocumentReference ref = db.collection("users").document(username);
+        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d( TAG, "Document exists!");
+                        usernameLabel.setText("@" + username);
+                        nameLabel.setText(document.get("name").toString());
+
+
+                    } else {
+
+                    }
+                }
+            }
+        });
+
+
+
+
+        v_habitList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if(isDeleting) {
-                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-                    String v_deletehabit = habitList.get(i).getHabitID();
-                    Log.d("TEST!", v_deletehabit);
-                    db.collection("Habits")
-                            .document(v_deletehabit)
-                            .delete()
-                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                @Override
-                                public void onSuccess(Void aVoid) {
-                                    Log.d("TAG", "City successfully deleted!");
-                                }
-                            })
-                            .addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.w("TAG", "Error deleting document", e);
-                                }
-                            });
-                    removeItemInList(i);
-                }
-                else {
-                    editHabit(habitList.get(i));
+                position = i;
 
-                }
+
+
+
             }
         });
 
-
-        /**
-         * This sets up the button click listener.
-         * When the button is clicked, it switches the
-         * boolean delete mode (T=Deleting, F=Not Deleting)
-         * and switches the button text accordingly.
-         */
-        deleteButton.setOnClickListener(new View.OnClickListener() {
+        deleteHabitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isDeleting = !isDeleting;
-                if(isDeleting)
-                    deleteButton.setText("Deleting");
-                else
-                    deleteButton.setText("Delete habit");
+
+                String v_deletehabit = habitList.get(position).getName();
+
+                //db.collection("Cities").document()
+                db.collection("Habits")
+
+                        .document(v_deletehabit)
+                        .delete()
+
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully deleted!" + v_deletehabit);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error deleting document", e);
+                            }
+                        });
+                populateList();
             }
         });
-    }
+    } //moved it to here
 
 
-    /**
-     * This function takes a habit, and adds it
-     * to the list, updating the adapter.
-     * Unfortunately I could not just add the item in
-     * populateList() because it wont work.
-     * @param habit
-     */
-    public void addHabitToList(Habit habit) {
+    public void addHabit(Habit habit) {
         habitList.add(habit);
         Log.d("LIST CHECK", habitList.get(0).getName());
-
         mainList = findViewById(R.id.habit_list);
-        //xml list reference
-
         habitAdapter = new CustomList(this, habitList);
         mainList.setAdapter(habitAdapter);
         habitAdapter.notifyDataSetChanged();
-        // update list
     }
 
-    /**
-     * This function looks inside the database,
-     * and gets the title, description, hour,
-     * minute, date, selected days, and username
-     * from the habit in the database, and
-     * stores those values into a habit object
-     * which then adds to the habitList
-     */
     public void populateList() {
+        final String TAG = "\nADDED:";
+//moved this
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Habits")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("HERE", "TESSTTTTT");
+                            habitList.clear();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String title = document.getData().get("name").toString();
+                                String description = document.getData().get("description").toString();
+                                String hour = document.getData().get("hour").toString();
+                                String minute = document.getData().get("minute").toString();
+                                String date = document.getData().get("date").toString();
+                                String selectedDays = document.getData().get("selectedDays").toString();
+                                String username = document.getData().get("username").toString();
+                                String habitID = document.getId();
 
+                                Habit newHabit = new Habit(title, description, selectedDays, hour, minute, date, username, habitID);
+                                addHabit(newHabit);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public void addHabit(View view) {
+        Intent intent = new Intent(this, AddActivity.class);
+        intent.putExtra("USERNAME", username);
+        startActivity(intent);
+
+        mainList = findViewById(R.id.habit_list);
+
+        habitAdapter = new CustomList(this, habitList);
+        mainList.setAdapter(habitAdapter);
+
+        final String TAG = "\nADDED:";
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("Habits")
@@ -167,8 +182,6 @@ public class SelfProfile extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         habitList.clear();
                         for (QueryDocumentSnapshot document : task.getResult()) {
-
-                            // Get the attributes from each habit in the database
                             String title = document.getData().get("name").toString();
                             String description = document.getData().get("description").toString();
                             String hour = document.getData().get("hour").toString();
@@ -176,71 +189,16 @@ public class SelfProfile extends AppCompatActivity {
                             String date = document.getData().get("date").toString();
                             String selectedDays = document.getData().get("selectedDays").toString();
                             String username = document.getData().get("username").toString();
+                            String habitID = document.getId();
 
-                            String id = document.getId();
-
-                            // Create new habit and add to the list!
-                            Habit newHabit = new Habit(title, description, selectedDays, hour, minute, date, username, id);
-                            addHabitToList(newHabit);
-
-                            Log.d("HABIT:", title);
-
+                            Habit newHabit = new Habit(title, description, selectedDays, hour, minute, date, username, habitID);
+                            addHabit(newHabit);
                         }
                     } else {
-                        Log.d("ERROR:", "Error getting documents: ", task.getException());
+                        Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
     }
-
-
-    /**
-     * This function starts an intent (AddActivity)
-     * and adds the habit to the database. This
-     * is called when "Add Habit" Button is
-     * tapped.
-     * @param view
-     */
-    public void addHabit(View view) {
-        // Start AddActivity Page
-        Intent intent = new Intent(this, AddActivity.class);
-        startActivity(intent);
-
-        Log.d("POPULATING:", "\n");
-    }
-
-    /**
-     * This function takes a habit, and puts
-     * it into a bundle, and passes into the
-     * AddActivity intent. The code there checks
-     * if the title is the same, and if it is
-     * it will apply the changes.
-     * @param habit
-     */
-    public void editHabit(Habit habit) {
-        Intent intent = new Intent(this, AddActivity.class);
-        intent.putExtra("data", habit);
-        startActivity(intent);
-    }
-
-    /**
-     * Given an index, will remove that item
-     * from the list, and update the adapter
-     * @param index
-     */
-    public void removeItemInList(int index) {
-        if (index >= 0) {
-            habitList.remove(index);
-            habitAdapter.notifyDataSetChanged();
-        }
-    }
-
-    /**
-     * Overrides onResume to update the list
-     * everytime the page is resumed.
-     */
-    @Override public void onResume() {
-        super.onResume();
-        populateList();
 
     // open up habit view activity for the habit at position i
     // in the habit list
@@ -249,7 +207,6 @@ public class SelfProfile extends AppCompatActivity {
         intent.putExtra("HABITID", habitList.get(i).getHabitID());
 
         startActivity(intent);
-
 
     }
 }
