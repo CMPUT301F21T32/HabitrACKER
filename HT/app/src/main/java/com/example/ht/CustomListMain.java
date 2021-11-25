69f92d2947881640fab4733861f7ec94e08c1570
anahita-gupta <anahita3@ualberta.ca>
2021-11-25 14:28:27 -0700

    main feed

---
+++ HT/app/src/main/java/com/example/ht/CustomListMain.java
@@ -0,0 +1,24 @@
+package com.example.ht;
+
+import android.widget.ArrayAdapter;
+
+
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+
+public class CustomListMain extends ArrayAdapter<Habit> {
+
+}
