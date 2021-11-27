cbedd9f65e689c616e5d785994d15c35eec4cf18
jacquelinedenise <91641561+jacquelinedenise@users.noreply.github.com>
2021-11-26 17:14:07 -0700

    Update ViewEventList.java

--- HT/app/src/main/java/com/example/ht/ViewEventList.java
+++ HT/app/src/main/java/com/example/ht/ViewEventList.java
@@ -2,6 +2,7 @@ package com.example.ht;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -9,6 +10,7 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -16,9 +18,15 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 
 public class ViewEventList extends AppCompatActivity {
-    String habitID;
+    String HabitID;
+    String eventID;
+    String eventName;
+    String eventDescription;
+    String eventComment;
+
     ListView eventList;
-    ArrayList<HabitEvent> habitevent= new ArrayList<>();
+    ArrayList<HabitEvent> habitEvent= new ArrayList<>();
+    ArrayAdapter<HabitEvent> eventAdapter= new PastEventList(this, habitEvent);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +36,16 @@ public class ViewEventList extends AppCompatActivity {
         // Connect to Firestore database
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Intent intent = getIntent();
-        habitID = intent.getStringExtra("HabitID");
+        HabitID = intent.getStringExtra("habitID");     // get habitID
 
         // Connect to activity page
         eventList= (ListView) findViewById(R.id.event_list);
 
         // Connect to HabitEvent object
-        habitevent= new ArrayList<>();
+        habitEvent= new ArrayList<>();
 
         // Get event list from Firestore
+
         db.collection("HabitEvents")
                 .get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -44,20 +53,50 @@ public class ViewEventList extends AppCompatActivity {
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
                         // Populate the list
                         if(!queryDocumentSnapshots.isEmpty()){
-                            for(QueryDocumentSnapshot doc : queryDocumentSnapshots){
-                                HabitEvent newEvent= doc.toObject(HabitEvent.class);        // convert doc to HabitEvent object
-                                habitevent.add(newEvent);                                   // add to list
+                            for(QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                                if (doc.getData().get("habitID") == HabitID) {
+                                    doc.getData().get("habitID");
+                                    HabitEvent newEvent = doc.toObject(HabitEvent.class);        // convert doc to HabitEvent object
+                                    habitEvent.add(newEvent);                                   // add to list
+                                }
                             }
+
+                            // Connect to list to display in app
+                            ArrayAdapter<HabitEvent> eventAdapter= new ArrayAdapter<HabitEvent>(getApplicationContext(), R.layout.event_cell, habitEvent);
+                            eventAdapter.notifyDataSetChanged();
+                            eventList.setAdapter(eventAdapter);
+
                         }
                     }
                 });
 
-        // Connect to list to display in app
-        for (int i=0; i<habitevent.size(); i++){
-            ArrayAdapter<HabitEvent> eventAdapter= new ArrayAdapter<HabitEvent>(habitevent.get(i).habitID);
-            eventAdapter.notifyDataSetChanged();
-            eventList.setAdapter(eventAdapter);
-        }
+        /**
+        db.collection("HabitEvents")
+                .get()
+                .addOnCompleteListener(task -> {
+                    habitEvent.clear();
+                    eventAdapter.notifyDataSetChanged();
+                    for(QueryDocumentSnapshot doc : task.getResult()){
+                        if (HabitID==doc.get("habitID").toString()){
+                            // Get HabitEvent values
+                            String eventID= doc.getData().get("habitID").toString();
+                            String eventName= doc.getData().get("name").toString();
+                            String eventComment= doc.get("comment").toString();
+                            String eventDescription= doc.get("reason").toString();
+                        }
+
+                        // Create new HabitEvent object and add it to the HabitEvent array
+                        HabitEvent newEvent= new HabitEvent(eventID, eventName, eventDescription, eventComment);
+                        habitEvent.add(newEvent);
+                    }
+
+                    // Connect to list to display in app
+                    eventAdapter.notifyDataSetChanged();
+                    Log.d("LIST CHECK", habitEvent.get(0).getName());
+
+                });
+        **/
+
 
         // When user clicks on event, go to details to edit/delete
         eventList.setOnClickListener(new View.OnClickListener() {
