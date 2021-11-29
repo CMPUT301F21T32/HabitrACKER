70f3084043dfb7c33ae69f168e3cb1cea358b255
jacquelinedenise <jazarcon@ualberta.ca>
2021-11-28 17:53:21 -0700

    Update

--- HT/app/src/main/java/com/example/ht/AddEventActivity.java
+++ HT/app/src/main/java/com/example/ht/AddEventActivity.java
@@ -1,10 +1,17 @@
 package com.example.ht;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+<<<<<<< HEAD
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -13,11 +20,19 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+=======
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+<<<<<<< HEAD
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,6 +41,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+=======
+import android.widget.ImageView;
+import android.widget.Toast;
+
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -33,7 +53,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import org.w3c.dom.Document;
+
+import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 
 public class AddEventActivity extends AppCompatActivity implements LocationListener, OnMapReadyCallback {
@@ -41,12 +67,18 @@ public class AddEventActivity extends AppCompatActivity implements LocationListe
     TextView latText;
     TextView lonText;
     Button addHabitEventButton;
+<<<<<<< HEAD
     Button cancelLocation;
+=======
+    Button uploadButton;
+    ImageView imageView;
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
     String comment;
     String habitID;
     String description;
     String name;
     String username;
+<<<<<<< HEAD
     double userLat;
     double userLon;
     String markerLat;
@@ -55,6 +87,11 @@ public class AddEventActivity extends AppCompatActivity implements LocationListe
     GoogleMap gMap;
 
     @SuppressLint("MissingPermission")
+=======
+    private StorageReference mStorageRef;
+    int CAMERA_REQUEST_CODE = 1;
+
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -63,6 +100,7 @@ public class AddEventActivity extends AppCompatActivity implements LocationListe
 
         habitEventDescription = findViewById(R.id.comment);
         addHabitEventButton = findViewById(R.id.add_habit_event);
+<<<<<<< HEAD
         cancelLocation = findViewById(R.id.cancellocation);
         latText = findViewById(R.id.lattext);
         lonText = findViewById(R.id.longtext);
@@ -81,24 +119,27 @@ public class AddEventActivity extends AppCompatActivity implements LocationListe
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+=======
+        uploadButton = findViewById(R.id.upload);
+        imageView = findViewById(R.id.eventImage);
+
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
 
         Intent intent = getIntent();
         habitID = intent.getStringExtra("HABITID");
 
-        DocumentReference ref = db.collection("Habits").document(habitID);
-        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful()){
-                    DocumentSnapshot document = task.getResult();
-                    if(document.exists()){
-                        description = document.get("description").toString();
-                        name = document.get("name").toString();
-                        username = document.get("username").toString();
-                    }
-                }
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
             }
         });
+<<<<<<< HEAD
 
         cancelLocation.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -131,19 +172,127 @@ public class AddEventActivity extends AppCompatActivity implements LocationListe
                             public void onSuccess(Void avoid){
                                 Log.d("AddHabitEvent", "HabitEventAddedSuccessfully");
                                 goToProfile();
+=======
+>>>>>>> b39f6f76ff005f938828bde3683392df7955692c
 
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d("AddHabitEvent", "Couldn't be added");
-                            }
-                        });
-            }
-
-        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] dataBAOS = baos.toByteArray();
+
+            imageView.setImageBitmap(bitmap);
+
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+            StorageReference filepath = mStorageRef.child("Photos").child("somename");
+
+            UploadTask uploadTask = filepath.putBytes(dataBAOS);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    DocumentReference ref = db.collection("Habits").document(habitID);
+                    ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if(task.isSuccessful()){
+                                DocumentSnapshot document = task.getResult();
+                                if(document.exists()){
+                                    description = document.get("description").toString();
+                                    name = document.get("name").toString();
+                                    username = document.get("username").toString();
+                                }
+                            }
+                        }
+                    });
+                    addHabitEventButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            comment = habitEventDescription.getText().toString();
+                            habitEventDescription.getText().clear();
+                            HashMap<String, String> data = new HashMap<>();
+                            data.put("habitID", habitID);
+                            data.put("name", name);
+                            data.put("comment", comment);
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            db.collection("HabitEvents")
+                                    .document()
+                                    .set(data)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            goToProfile();
+                                        }
+                                    });
+
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+
+    //        DocumentReference ref = db.collection("Habits").document(habitID);
+//        ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if(task.isSuccessful()){
+//                    DocumentSnapshot document = task.getResult();
+//                    if(document.exists()){
+//                        description = document.get("description").toString();
+//                        name = document.get("name").toString();
+//                        username = document.get("username").toString();
+//                    }
+//                }
+//            }
+//        });
+//
+//        addHabitEventButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view){
+//                comment = habitEventDescription.getText().toString();
+//                habitEventDescription.getText().clear();
+//                HashMap<String, String> data = new HashMap<>();
+//                Log.d("comment", comment);
+//                data.put("habitID", habitID);
+//                data.put("name", name);
+//                data.put("comment", comment);
+//                // Log.d("Comment", comment);
+//                FirebaseFirestore db = FirebaseFirestore.getInstance();
+//                db.collection("HabitEvents")
+//                        .document()
+//                        .set(data)
+//                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                            @Override
+//                            public void onSuccess(Void avoid){
+//                                Log.d("AddHabitEvent", "HabitEventAddedSuccessfully");
+//                                goToProfile();
+//
+//                            }
+//                        })
+//                        .addOnFailureListener(new OnFailureListener() {
+//                            @Override
+//                            public void onFailure(@NonNull Exception e) {
+//                                Log.d("AddHabitEvent", "Couldn't be added");
+//                            }
+//                        });
+//            }
+//
+//        });
+//    }
 
     /**
      * returns app to profile activity
