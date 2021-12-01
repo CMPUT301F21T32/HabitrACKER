b7d57edd3c18f9da70e5daf35b9ca4bb6fa32e3d
jacquelinedenise <91641561+jacquelinedenise@users.noreply.github.com>
2021-12-01 02:37:21 -0700

    Update

--- HT/app/src/main/java/com/example/ht/ViewHabitActivity.java
+++ HT/app/src/main/java/com/example/ht/ViewHabitActivity.java
@@ -1,4 +1,44 @@
 /**
+ * ViewHabitActivity
+ * @author cole
+ *
+ * creates a screen that allows you to view the details of a habit
+ *
+ * This activity should be passed as habitID from the previous activity
+ */
+
+package com.example.ht;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+/**
  * this creates the page that allows you to view the details of a habit
  *
  * @aurhor cole
