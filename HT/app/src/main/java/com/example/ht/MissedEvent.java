fd0ca21579fe3eec313723db6eccbdc6107311af
jacquelinedenise <91641561+jacquelinedenise@users.noreply.github.com>
2021-12-01 00:57:11 -0700

    Update

--- HT/app/src/main/java/com/example/ht/MissedEvent.java
+++ HT/app/src/main/java/com/example/ht/MissedEvent.java
@@ -1,8 +1,8 @@
 package com.example.ht;
 
 public class MissedEvent extends HabitEvent{
-    public MissedEvent(String habitID, String name, String description) {
-        super(habitID, name, description);
+    public MissedEvent(String habitID) {
+        super(habitID);
     }
     // alerts user when they miss an event
 }
